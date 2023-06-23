6814ac39d7aacb4a0360d2c6a9f2029b7875ce5c
Karolis <karolisdarbinis2@gmail.com>
2023-06-23 17:36:11 +0300

    Update

---
+++ js/main.js
@@ -0,0 +1,69 @@
+console.clear();
+
+// Styling an Element
+
+// const title = document.querySelector('.main-heading');
+
+// title.style.color = 'red'; //works in a single element
+
+// const listItems = document.querySelectorAll('.list-items');
+
+// for(let i = 0; i < listItems.length; i++){
+//     listItems[i].style.fontSize = '2rem'; //visiem list itemams priskiria
+// }
+
+//Creating elements
+
+// const ul = document.querySelector('ul');
+// const li = document.createElement('li');
+
+// //Adding element
+// ul.append(li);
+
+// //Modifying element
+// li.innerText = 'Hello';
+
+// const firstNumber = document.querySelector('span');
+
+// console.log(firstNumber);
+
+// //Modifying Attributes & Classes
+
+// // li.setAttribute('class', 'number');
+// li.setAttribute('id', 'main-heading');
+// li.removeAttribute('id');
+
+// //Kitas budas pasiekti atributus
+
+// const title = document.querySelector('#main-heading');
+
+// console.log(title.getAttribute('id'));
+
+//Classes
+
+// li.classList.add('list-items');
+// li.classList.remove('list-items');
+
+//Contains(method)
+
+// console.log(li.classList.contains('list-items'));
+
+//Remove Elements
+
+// li.remove();
+
+//Parent Node Traversal
+
+// let ul = document.querySelector('ul');
+
+// console.log(ul.parentNode);
+// console.log(ul.parentNode.parentNode);
+// console.log(ul.parentElement.parentElement);
+
+// const html = document.documentElement;
+
+// console.log(html.parentNode);
+// console.log(html.parentElement);
+
+let ul = document.querySelector('ul');
+
