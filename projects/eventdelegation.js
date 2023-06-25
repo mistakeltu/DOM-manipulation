b90d0715f2ff54a25c07c8dd3bb87729a83908f4
Karolis <karolisdarbinis2@gmail.com>
2023-06-25 23:45:23 +0300

    Update

--- projects/eventdelegation.js
+++ projects/eventdelegation.js
@@ -2,14 +2,35 @@ console.clear();
 
 // Event Delegation
 
-document.querySelector('#football').addEventListener('click', function(e){
-    console.log('Paspausta');
 
+//Ilgesnis budas
+// document.querySelector('#football').addEventListener('click', function(e){
+//     console.log('Paspausta');
+
+//     const target = e.target;
+
+//     console.log(target);
+
+//     if(target.matches('li')){
+//         target.style.backgroundColor = 'grey';
+//     }
+// })
+
+//Trumpesnis
+
+document.querySelector('#stuff').addEventListener('click', function(e){
+    console.log(e.target.getAttribute('id') + ' is clicked');
     const target = e.target;
-
-    //console.log(target);
-
+    //  console.log(target);
     if(target.matches('li')){
-        target.style.backgroundColor = 'grey';
-    }
+      target.style.backgroundColor = 'grey';
+}
 })
+
+const stuff = document.querySelector('#sports');
+const newStuff = document.querySelector('li');
+
+newStuff.innerText = 'wassup';
+newStuff.setAttribute('id', 'wassup');
+
+//stuff.appendChild(newStuff);
